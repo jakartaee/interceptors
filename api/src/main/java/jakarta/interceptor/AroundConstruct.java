45664f8973a78dea98986a60695c5554e1c7c7e2
arjantijms <arjan.tijms@gmail.com>
2020-07-20 18:02:47 +0200

    Update copyright year
    Signed-off-by: arjantijms <arjan.tijms@gmail.com>

--- api/src/main/java/jakarta/interceptor/AroundConstruct.java
+++ api/src/main/java/jakarta/interceptor/AroundConstruct.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013, 2018 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2013, 2020 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -24,7 +24,7 @@ import java.lang.annotation.Target;
 
 /**
  * Designates an interceptor method that receives a callback when the target class constructor is invoked.
- * 
+ *
  * <p>
  * The method to which the <code>AroundConstruct</code> annotation is applied must have one of the following signatures.
  * </p>
--- api/src/main/java/jakarta/interceptor/Interceptor.java
+++ api/src/main/java/jakarta/interceptor/Interceptor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1997, 2018 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997, 2020 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -96,7 +96,7 @@ public @interface Interceptor {
      * interceptors bound to a target using the {@link Interceptors Interceptors} annotation.
      *
      * @see jakarta.annotation.Priority
-     * 
+     *
      * @since Jakarta Interceptors 1.2
      */
     public static class Priority {
